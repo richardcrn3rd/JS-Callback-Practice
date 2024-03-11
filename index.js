8c36b5ee0a5fcc7d4d150cc83ef746a5609b4cf4
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-03-11 00:14:34 -0400

    Final Commit Index withNew Image

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-function handleDirectionChange(direction){
-    if(direction === null){
+function handleDirectionChange(direction) {
+    if (direction === null) {
         character.src = `assets/green-character/static.gif`
     }
-    if(direction === 'west'){
+    if (direction === 'west') {
         character.src = `assets/green-character/west.gif`
     }
-    if(direction === 'north'){
+    if (direction === 'north') {
         character.src = `assets/green-character/north.gif`
     }
-    if(direction === 'east'){
+    if (direction === 'east') {
         character.src = `assets/green-character/east.gif`
     }
-    if(direction === 'south'){
+    if (direction === 'south') {
         character.src = `assets/green-character/south.gif`
     }
 }
@@ -23,11 +23,12 @@ function handleDirectionChange(direction){
 move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
-move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/tree.png')).to(700, 400)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
 move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/sword.png')).to(100, 500)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
